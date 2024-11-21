513219d7808169f5192348a859e9bea1757eb997
Graziela00 <124847003+Graziela00@users.noreply.github.com>
2024-11-20 23:56:14 -0300

    Update CidadeTest.java

--- src/test/java/exercicios/CidadeTest.java
+++ src/test/java/exercicios/CidadeTest.java
@@ -7,8 +7,23 @@ import java.util.Map;
 /**
  * @author Manoel Campos
  */
-class CidadeTest extends ClassTest {
-    CidadeTest() {
-        super(Map.of("nome", "String"));
+// class CidadeTest extends ClassTest {
+//     CidadeTest() {
+//         super(Map.of("nome", "String"));
+//     }
+// }
+public class Cidade {
+    private String nome;
+
+    public Cidade(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String novoNome) {
+        this.nome = novoNome;
     }
 }

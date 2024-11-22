8f22e7a72f32f1576486865a90c221798eb8b773
Graziela00 <124847003+Graziela00@users.noreply.github.com>
2024-11-21 22:00:20 -0300

    Update CidadeTest.java

--- src/test/java/exercicios/CidadeTest.java
+++ src/test/java/exercicios/CidadeTest.java
@@ -1,14 +1,29 @@
-package exercicios;
+// package exercicios;
 
-import io.github.manoelcampos.examcorrection.ClassTest;
+// import io.github.manoelcampos.examcorrection.ClassTest;
 
-import java.util.Map;
+// import java.util.Map;
 
-/**
- * @author Manoel Campos
- */
-class CidadeTest extends ClassTest {
-    CidadeTest() {
-        super(Map.of("nome", "String"));
+// /**
+//  * @author Manoel Campos
+//  */
+// class CidadeTest extends ClassTest {
+//     CidadeTest() {
+//         super(Map.of("nome", "String"));
+//     }
+// }
+public class Cidade {
+    private String nome;
+
+    public Cidade(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String novoNome) {
+        this.nome = novoNome;
     }
 }

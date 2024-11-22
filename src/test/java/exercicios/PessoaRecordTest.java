6ef66355fc58653ceb19aa43e9ad2592061e7382
Graziela00 <124847003+Graziela00@users.noreply.github.com>
2024-11-21 22:01:12 -0300

    Update PessoaRecordTest.java

--- src/test/java/exercicios/PessoaRecordTest.java
+++ src/test/java/exercicios/PessoaRecordTest.java
@@ -1,14 +1,16 @@
-package exercicios;
+// package exercicios;
 
-import io.github.manoelcampos.examcorrection.RecordTest;
+// import io.github.manoelcampos.examcorrection.RecordTest;
 
-import java.util.Map;
+// import java.util.Map;
 
-/**
- * @author Manoel Campos
- */
-class PessoaRecordTest extends RecordTest {
-    PessoaRecordTest() {
-        super(Map.of("nome", "String", "cidade", "CidadeRecord"));
-    }
-}
+// /**
+//  * @author Manoel Campos
+//  */
+// class PessoaRecordTest extends RecordTest {
+//     PessoaRecordTest() {
+//         super(Map.of("nome", "String", "cidade", "CidadeRecord"));
+//     }
+// }
+
+public record PessoaRecord(String nome, CidadeRecord cidade) {}

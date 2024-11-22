b6efa4b1d77319cbfcf7b22e3180f5ce2cb1e602
Graziela00 <124847003+Graziela00@users.noreply.github.com>
2024-11-21 22:02:11 -0300

    Update PessoaRecordShallowTest.java

--- src/test/java/exercicios/PessoaRecordShallowTest.java
+++ src/test/java/exercicios/PessoaRecordShallowTest.java
@@ -1,14 +1,17 @@
-package exercicios;
+// package exercicios;
 
-import io.github.manoelcampos.examcorrection.RecordTest;
+// import io.github.manoelcampos.examcorrection.RecordTest;
 
-import java.util.Map;
+// import java.util.Map;
 
-/**
- * @author Manoel Campos
- */
-class PessoaRecordShallowTest extends RecordTest {
-    PessoaRecordShallowTest() {
-        super(Map.of("nome", "String", "cidade", "Cidade"));
-    }
-}
+// /**
+//  * @author Manoel Campos
+//  */
+// class PessoaRecordShallowTest extends RecordTest {
+//     PessoaRecordShallowTest() {
+//         super(Map.of("nome", "String", "cidade", "Cidade"));
+//     }
+// }
+
+public record PessoaRecordShallow(String nome, Cidade cidade) {}
+
